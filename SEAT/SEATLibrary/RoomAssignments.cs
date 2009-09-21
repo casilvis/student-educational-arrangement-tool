8040d4a0a43c732c17d4be30795e3862ecbb1c5c
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2009-09-21 20:33:02 +0000

    Added "blank" classes for all of the objects that are represented in the database.

---
+++ SEAT/SEATLibrary/RoomAssignments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEATLibrary
+{
+    class RoomAssignments
+    {
+        private int primaryId;
+        private Student student;
+        private Room room;
+
+    }
+}
---
+++ SEAT/SEATLibrary/RoomType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEATLibrary
+{
+    class RoomType
+    {
+        private int primaryId;
+        private String roomTypeName;
+
+    }
+}
