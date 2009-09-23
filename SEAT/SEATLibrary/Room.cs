a9eb503e9fcbbeac2774bd9aa0777f605c59be71
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2009-09-23 14:50:14 +0000

    Added some basic implementations of the various base classes that will be used in the application and included a simple console application that can be used for testing.

--- SEAT/SEATLibrary/Room.cs
+++ SEAT/SEATLibrary/Room.cs
@@ -5,13 +5,107 @@ using System.Text;
 
 namespace SEATLibrary
 {
-    class Room
+    public class Room
     {
-        private int primaryId;
-        private RoomType roomType;
+        //private int primaryId;
+        //private RoomType roomType;
         private String roomName;
         private String location;
         private String description;
+
+        private List<Student> roomStudents;
+        private Chair[,] chairs;
+        private int width;
+        private int height;
+
+        public String RoomName
+        {
+            get { return roomName; }
+            set { roomName = value; }
+        }
+        public String Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Chair[,] Chairs
+        {
+            get { return chairs; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+        }
+        public int Height
+        {
+            get { return height; }
+        }
+        public List<Student> RoomStudents
+        {
+            get { return roomStudents; }
+        }
+
+
+        public Room()
+        {
+            this.width = 4;
+            this.height = 4;
+
+            this.roomName = "Unknown";
+            this.location = "Unknown";
+            this.description = "Unknown";
+            roomStudents = new List<Student>();
+            chairs = new Chair[this.width, this.height];
+
+            for (int i = 0; i < this.width; i++)
+            {
+                for (int j = 0; j < this.height; j++)
+                {
+                    chairs[i, j] = new Chair();
+                }
+            }
+        }
+
+        public Room(String roomName, String location, String description, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            this.roomName = roomName;
+            this.location = location;
+            this.description = description;
+            roomStudents = new List<Student>();
+            chairs = new Chair[this.width, this.height];
+
+            for (int i = 0; i < this.width; i++)
+            {
+                for (int j = 0; j < this.height; j++)
+                {
+                    chairs[i, j] = new Chair();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            String result = "";
+            for (int i = 0; i < this.width; i++)
+            {
+                for (int j = 0; j < this.height; j++)
+                {
+                    result += chairs[i, j].ToString() + "\t";
+                }
+                result += "\n";
+            }
+            return result;
+        }
 
     }
 }
--- SEAT/SEATLibrary/Student.cs
+++ SEAT/SEATLibrary/Student.cs
@@ -7,7 +7,7 @@ namespace SEATLibrary
 {
     public class Student
     {
-        private int primaryId;
+        //private int primaryId;
         private String firstName;
         private String lastName;
         private String section;
@@ -15,5 +15,45 @@ namespace SEATLibrary
         private Boolean visionEnpairment;
         private Boolean isEnrolled;
 
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public String Section
+        {
+            get { return section; }
+            set { section = value; }
+        }
+        public Boolean LeftHanded
+        {
+            get { return leftHanded; }
+            set { leftHanded = value; }
+        }
+        public Boolean VisionEnpairment
+        {
+            get { return visionEnpairment; }
+            set { visionEnpairment = value; }
+        }
+        public Boolean IsEnrolled
+        {
+            get { return isEnrolled; }
+            set { isEnrolled = value; }
+        }
+
+        public Student()
+        {
+            firstName = "";
+            lastName = "";
+            section = "";
+            leftHanded = false;
+            visionEnpairment = false;
+            isEnrolled = false;
+        }
     }
 }
