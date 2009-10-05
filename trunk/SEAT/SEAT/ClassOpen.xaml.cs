41feffbdf8368ee13a113af75f3221fd29b38551
jradam03 <jradam03@3651bf16-9bd1-11de-8050-81a99a42e534>
2009-10-05 13:43:27 +0000

    Rearranged GUI's to interact properly and added a few more

---
+++ trunk/SEAT/SEAT/ClassOpen.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace SEAT
+{
+    /// <summary>
+    /// Interaction logic for ClassOpen.xaml
+    /// </summary>
+    public partial class ClassOpen : Window
+    {
+        public ClassOpen()
+        {
+            InitializeComponent();
+        }
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)rbtncreate.IsChecked)
+            {
+                string row = Regex.Replace(txtRows.Text, @"[\D]", "");
+                string column = Regex.Replace(txtColumns.Text, @"[\D]", "");
+                txtRows.Text = row;
+                txtColumns.Text = column;
+                if (column.Length > 0 && row.Length > 0)
+                {
+                    frmGrid Grid = new frmGrid(Convert.ToInt32(row), Convert.ToInt32(column));
+                    Grid.Show();
+                    this.Close();
+                }
+            }
+        }
+    }
+}
