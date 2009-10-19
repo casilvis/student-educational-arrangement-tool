bc521ec560a0c6eb65fe011ad3ade5b4c370667a
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2009-10-19 14:26:48 +0000

    Student information can now be edited through the interface.

--- SEAT/SEAT/StudentAdd.xaml.cs
+++ SEAT/SEAT/StudentAdd.xaml.cs
@@ -20,24 +20,64 @@ namespace SEAT
     /// </summary>
     public partial class StudentAdd : Window
     {
+        Student student;
+
         public StudentAdd()
         {
             InitializeComponent();
+            this.student = null;
             txtLName.Focus();
 
+        }
+
+        public StudentAdd(Student student)
+        {
+            InitializeComponent();
+            this.student = student;
+            txtFName.Text = student.FirstName;
+            txtLName.Text = student.LastName;
+            txtSID.Text = student.Sid;
+            txtSection.Text = student.Section;
+            if (student.LeftHanded)
+            {
+                rbtnLeft.IsChecked = true;
+            }
+            else
+            {
+                rbtnRight.IsChecked = true;
+            }
+            if (student.VisionImpairment)
+            {
+                chkbxvision.IsChecked = true;
+            }
+            txtLName.Focus();
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             if (txtSID.Text.Length > 0 && txtLName.Text.Length > 0 && txtFName.Text.Length > 0)
             {
-                Student student = new Student(txtFName.Text, txtLName.Text, txtSID.Text, 
-                    txtSection.Text, (bool)rbtnLeft.IsChecked, (bool)chkbxvision.IsChecked);
-                Window1.manager.addStudentToRoster(student);
+                if (student == null)
+                {
+                    student = new Student(txtFName.Text, txtLName.Text, txtSID.Text,
+                        txtSection.Text, (bool)rbtnLeft.IsChecked, (bool)chkbxvision.IsChecked);
+                    Window1.manager.addStudentToRoster(student);
+                }
+                else
+                {
+                    student.FirstName = txtFName.Text;
+                    student.LastName = txtLName.Text;
+                    student.Sid = txtSID.Text;
+                    student.Section = txtSection.Text;
+                    student.LeftHanded = (bool)rbtnLeft.IsChecked;
+                    student.VisionImpairment = (bool)chkbxvision.IsChecked;
+                }
                 this.Close();
             }
             else
+            {
                 MessageBox.Show("make sure to set all values");
+            }
         }
     }
 }
