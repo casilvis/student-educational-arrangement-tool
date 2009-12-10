24fcb4e79b94bedeb4d4afca32d784bb5f329273
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2009-12-10 18:57:05 +0000

    Roster import has been enhanced to allow the user to properly select the columns that match the imported data.  Additionally, the import is not performed until the user explicitely selects to import the data.

--- SEAT/SEAT/RosterImporter.xaml.cs
+++ SEAT/SEAT/RosterImporter.xaml.cs
@@ -5,6 +5,7 @@ namespace SEAT
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using System.Windows;
@@ -33,6 +34,16 @@ namespace SEAT
         private bool isOpened;
 
         /// <summary>
+        /// The column selection boxes that allow the user to choose what each column represents.
+        /// </summary>
+        private ComboBox[] columns;
+
+        /// <summary>
+        /// The first row of data that allows the user to choose if it represents column headers.
+        /// </summary>
+        private TextBlock[] headers;
+
+        /// <summary>
         /// Initializes a new instance of the RosterImporter class.
         /// </summary>
         public RosterImporter()
@@ -58,6 +69,7 @@ namespace SEAT
                 }
 
                 this.isOpened = true;
+                this.myGrid.Background = Brushes.White;
                 this.FillGrid();
             }
             else
@@ -88,31 +100,183 @@ namespace SEAT
             }
 
             // Add all of the necessary rows
-            for (int i = 0; i < this.rosterFile.Count; i++)
+            for (int i = 0; i < this.rosterFile.Count + 1; i++)
             {
                 RowDefinition rd = new RowDefinition();
                 rd.Height = new GridLength(20);
                 myGrid.RowDefinitions.Add(rd);
             }
 
+            // Set up combo boxes
+            this.columns = new ComboBox[this.rosterFile.NumColumns];
+
+            // Add the selection menus
+            for (int i = 0; i < this.rosterFile.NumColumns; i++)
+            {
+                ComboBox cbox = new ComboBox();
+                this.columns[i] = cbox;
+                cbox.Background = Brushes.LightBlue;
+                ComboBoxItem cboxitem = new ComboBoxItem();
+                cboxitem.Content = "First Name";
+                cbox.Items.Add(cboxitem);
+                ComboBoxItem cboxitem2 = new ComboBoxItem();
+                cboxitem2.Content = "Last Name";
+                cbox.Items.Add(cboxitem2);
+                ComboBoxItem cboxitem3 = new ComboBoxItem();
+                cboxitem3.Content = "Student ID";
+                cbox.Items.Add(cboxitem3);
+                ComboBoxItem cboxitem4 = new ComboBoxItem();
+                cboxitem4.Content = "Section";
+                cbox.Items.Add(cboxitem4);
+                Grid.SetRow(cbox, 0);
+                Grid.SetColumn(cbox, i);
+                myGrid.Children.Add(cbox);
+            }
+
             // Add all of the data into the grid
-            for (int i = 1; i < this.rosterFile.Count; i++)
+            for (int i = 0; i < this.rosterFile.Count; i++)
             {
                 string[] cols = this.rosterFile.ParsedData[i];
-                Student student = new Student(cols[2], cols[1], cols[0], cols[4], false, false);
-                Window1.SManager.AddStudentToRoster(student);
+                if (i == 0)
+                {
+                    this.headers = new TextBlock[cols.Length];
+                }
+
                 for (int j = 0; j < cols.Length; j++)
                 {
                     TextBlock cell = new TextBlock();
                     cell.Text = cols[j];
                     cell.FontSize = 11;
                     cell.VerticalAlignment = VerticalAlignment.Top;
-                    Grid.SetRow(cell, i);
+                    Grid.SetRow(cell, i + 1);
                     Grid.SetColumn(cell, j);
-
                     myGrid.Children.Add(cell);
+                    if (i == 0)
+                    {
+                        this.headers[j] = cell;
+                    }
                 }
             }
+
+            // Update the number of students
+            this.labelNumberOfStudents.Content = "Number of Students: " + this.rosterFile.Count.ToString();
+        }
+
+        /// <summary>
+        /// The first row is a heading.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">Event arguments.</param>
+        private void CheckBoxHeaders_Checked(object sender, RoutedEventArgs e)
+        {
+            this.labelNumberOfStudents.Content = "Number of Students: " + (this.rosterFile.Count - 1).ToString();
+            if (this.rosterFile.Count > 0)
+            {
+                for (int i = 0; i < this.headers.Length; i++)
+                {
+                    this.headers[i].Background = Brushes.LightGray;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The first row is not a heading.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">Event arguments.</param>
+        private void CheckBoxHeaders_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.labelNumberOfStudents.Content = "Number of Students: " + this.rosterFile.Count.ToString();
+            if (this.rosterFile.Count > 0)
+            {
+                for (int i = 0; i < this.headers.Length; i++)
+                {
+                    this.headers[i].Background = Brushes.White;
+                }
+            }
+        }
+
+        /// <summary>
+        /// If the columns are properly selected, add the students and close the window.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">Event arguments.</param>
+        private void ButtonImport_Click(object sender, RoutedEventArgs e)
+        {
+            int start = 0;
+            if (this.checkBoxHeaders.IsChecked.Value && this.rosterFile.Count > 0)
+            {
+                start++;
+            }
+
+            if (this.ValidateColumnHeaders())
+            {
+                for (int i = start; i < this.rosterFile.Count; i++)
+                {
+                    string[] cols = this.rosterFile.ParsedData[i];
+                    string firstname = string.Empty;
+                    string lastname = string.Empty;
+                    string studentid = string.Empty;
+                    string section = string.Empty;
+
+                    for (int j = 0; j < cols.Length; j++)
+                    {
+                        if (this.columns[j].SelectedIndex == 0)
+                        {
+                            firstname = cols[j];
+                        }
+                        else if (this.columns[j].SelectedIndex == 1)
+                        {
+                            lastname = cols[j];
+                        }
+                        else if (this.columns[j].SelectedIndex == 2)
+                        {
+                            studentid = cols[j];
+                        }
+                        else if (this.columns[j].SelectedIndex == 3)
+                        {
+                            section = cols[j];
+                        }
+                    }
+
+                    Student student = new Student(firstname, lastname, studentid, section, false, false);
+                    Window1.SManager.AddStudentToRoster(student);
+                }
+            }
+
+            this.Close();
+        }
+
+        /// <summary>
+        /// Performs a check to make sure each of the headers was selected only once.
+        /// </summary>
+        /// <returns>True if a valid column header configuration is selected.</returns>
+        private bool ValidateColumnHeaders()
+        {
+            int[] test = new int[4];
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = 0;
+            }
+
+            for (int i = 0; i < this.columns.Length; i++)
+            {
+                if (this.columns[i].SelectedIndex >= 0)
+                {
+                    test[this.columns[i].SelectedIndex]++;
+                }
+            }
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (test[i] != 1)
+                {
+                    MessageBox.Show("Invalid column selection.");
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
