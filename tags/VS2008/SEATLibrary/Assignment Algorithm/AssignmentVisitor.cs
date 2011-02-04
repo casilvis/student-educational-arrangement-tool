163a49b2f13fe9d960704db96632d65b555abdb4
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2011-02-04 14:56:49 +0000

    Tagged the version of the code before the migration to Visual Studio 2010.

---
+++ tags/VS2008/SEATLibrary/Assignment Algorithm/AssignmentVisitor.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="AssignmentVisitor.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>Assignment visitor algorithm for placing students in a room.</summary>
+namespace SEATLibrary.Assignment_Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Assignment visitor algorithm for placing students in a room.
+    /// </summary>
+    public abstract class AssignmentVisitor
+    {
+        /// <summary>
+        /// A random number generator used for randomizing the list of students.
+        /// </summary>
+        private static Random r = new Random();
+
+        /// <summary>
+        /// Gets the Random number generator.
+        /// </summary>
+        /// <value>The Random number generator.</value>
+        internal static Random R
+        {
+            get { return r; }
+        }
+
+        /// <summary>
+        /// Algorithm that will modify the room by placing students into seats.
+        /// </summary>
+        /// <param name="room">Room to be modified.</param>
+        public abstract void PlaceStudents(Room room);
+    }
+}
