e286672524609012235937cb81475f5f1c2f8ef9
jjhatf02 <jjhatf02@3651bf16-9bd1-11de-8050-81a99a42e534>
2011-04-06 15:00:40 +0000

    Tagged the 1.2.0.0 release.

---
+++ tags/SEAT_1_2_0_0/SEATLibrary/Assignment Algorithm/AssignmentClearRoom.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="AssignmentClearRoom.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>Remove all students from the seats in a room.</summary>
+namespace SEATLibrary.Assignment_Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Removes all students from the seats in a room.
+    /// </summary>
+    public class AssignmentClearRoom : AssignmentVisitor
+    {
+        /// <summary>
+        /// Algorithm for PlaceStudents that removes all students currently in chairs from their chair.
+        /// </summary>
+        /// <param name="room">The room to be modified.</param>
+        public override void PlaceStudents(Room room)
+        {
+            for (int i = 0; i < room.Height; i++)
+            {
+                for (int j = 0; j < room.Width; j++)
+                {
+                    room.Chairs[i, j].TheStudent = null;
+                }
+            }
+        }
+    }
+}
